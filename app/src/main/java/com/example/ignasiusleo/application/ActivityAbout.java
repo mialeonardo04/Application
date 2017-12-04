d03ea465df016aa6d8a006ddb1bbf43bf2c0b6b2
leonard04 <ignasiusleonardo@gmail.com>
2017-12-04 18:19:03 +0700

    Create README.md

---
+++ app/src/main/java/com/example/ignasiusleo/application/ActivityAbout.java
@@ -0,0 +1,13 @@
+package com.example.ignasiusleo.application;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
