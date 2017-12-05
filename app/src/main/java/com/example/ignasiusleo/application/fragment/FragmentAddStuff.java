f8849cf448f21896853aa7b67b1f2fb0e36b34bd
leonard04 <ignasiusleonardo@gmail.com>
2017-12-05 15:40:23 +0700

    Create README.md

--- app/src/main/java/com/example/ignasiusleo/application/fragment/FragmentAddStuff.java
+++ app/src/main/java/com/example/ignasiusleo/application/fragment/FragmentAddStuff.java
@@ -3,16 +3,22 @@ package com.example.ignasiusleo.application.fragment;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ignasiusleo.application.R;
+import com.example.ignasiusleo.application.model.DataHelper;
 
 import info.vividcode.android.zxing.CaptureActivity;
 import info.vividcode.android.zxing.CaptureActivityIntents;
@@ -21,7 +27,11 @@ import info.vividcode.android.zxing.CaptureActivityIntents;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentAddStuff extends Fragment {
+    protected Cursor cursor;
     TextView txtId;
+    DataHelper dbHelper;
+    Button save, cancel;
+    EditText namaBrg, hargaBrg, tgl_masuk, tgl_expired, jumlah_brg, ket;
 
     public FragmentAddStuff() {
         // Required empty public constructor
@@ -41,6 +51,34 @@ public class FragmentAddStuff extends Fragment {
                 Intent capture = new Intent(getContext(), CaptureActivity.class);
                 CaptureActivityIntents.setPromptMessage(capture, "Scanning");
                 startActivityForResult(capture, 0);
+            }
+        });
+        dbHelper = new DataHelper(getActivity());
+        namaBrg = v.findViewById(R.id.nama);
+        hargaBrg = v.findViewById(R.id.harga);
+        tgl_masuk = v.findViewById(R.id.tgl_masuk);
+        tgl_expired = v.findViewById(R.id.tgl_expired);
+        jumlah_brg = v.findViewById(R.id.jumlah);
+        save = v.findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                String sqlInsertBarang = "insert into barang(";
+                String sqlInsertStock = "";
+                db.execSQL(sqlInsertBarang);
+            }
+        });
+        cancel = v.findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment3 = new FragmentThree();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_content, fragment3);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
         return v;
--- app/src/main/java/com/example/ignasiusleo/application/model/DataHelper.java
+++ app/src/main/java/com/example/ignasiusleo/application/model/DataHelper.java
@@ -3,6 +3,7 @@ package com.example.ignasiusleo.application.model;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by ignasiusleo on 05/12/17.
@@ -12,6 +13,10 @@ public class DataHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "mokas.db";
     private static final int DATABASE_VERSION = 1;
+    private static final String TABEL_BARANG = "barang";
+    private static final String TABEL_STOCK = "stock";
+    private static final String TABEL_PENJUALAN = "penjualan";
+    private static final String TABEL_TRANSAKSI = "transaksi";
 
     public DataHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -19,11 +24,26 @@ public class DataHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sqlBarang = "create table barang(id_barang text primary key,nama_barang text null, harga_barang integer null, t";
+        String sqlBarang = "create table " + TABEL_BARANG + "(id_barang text PRIMARY KEY NOT NULL,nama_barang text null, harga_barang text null, keterangan text null);";
+        String sqlStock = "create table " + TABEL_STOCK + "(id_stock INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,id_barang text PRIMARY KEY NOT NULL,jumlah text not nul, tgl_datang text null, tgl_kadaluarsa text null);";
+        String sqlPenjualan = "create table " + TABEL_PENJUALAN + "(id_transaksi INTEGER not null,id_barang text not null, jumlah text null);";
+        String sqlTransaksi = "create table " + TABEL_TRANSAKSI + "(id_transaksi INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, jumlah text null)";
+
+        Log.d("Tabel Barang", "onCreate" + sqlBarang);
+        Log.d("Tabel Stock", "onCreate" + sqlStock);
+        Log.d("Tabel Penjualan", "onCreate" + sqlPenjualan);
+        Log.d("Tabel Transaksi", "onCreate" + sqlTransaksi);
+
+        sqLiteDatabase.execSQL(sqlBarang);
+        sqLiteDatabase.execSQL(sqlStock);
+        sqLiteDatabase.execSQL(sqlPenjualan);
+        sqLiteDatabase.execSQL(sqlTransaksi);
+
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
 
     }
 }
